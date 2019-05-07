2df96463744c3e0920e967235400fb238ea920fa
lvjie <lvjie@mosainet.com>
2019-05-07 17:11:19 +0800

    样式

--- uiinspect/src/views/index.vue
+++ uiinspect/src/views/index.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>4</div>
+  <div class="Selection">
+    4
+    <el-button class="iconempty" circle></el-button>
+    <el-button  type="warning" icon="el-icon-check" circle></el-button>
+  </div>
 </template>
+<style scoped>
+.iconempty {
+  height: 20px;
+  width: 20px;
+}
+</style>
